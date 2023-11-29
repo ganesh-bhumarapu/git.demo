552c1e276e1bd818d692279090ea2475f28a1f10
ganesh <141494560+ganesh9966-boss@users.noreply.github.com>
2023-11-29 18:54:55 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize an array
+        int[] myArray = {1, 2, 3, 4, 5};
+
+        // Print each element of the array
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println("Element at index " + i + ": " + myArray[i]);
+        }
+    }
+}
